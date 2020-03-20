75804433eb99fc1ab556babf9bf178d990baf3d2
Jack Cullen <jpcullen20@gmail.com>
2020-03-19 19:01:12 -0600

    Add instance vars to turn class test they are readable

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,10 @@
 class Turn
-
+  attr_reader :player1, :player2, :spoils_of_war, :winner
   def initialize(player1, player2)
-
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+    @winner = nil
   end
 
 
