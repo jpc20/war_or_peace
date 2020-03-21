422cb927aea6c7a6d89058f17ef60ff69d63dd6a
Jack Cullen <jpcullen20@gmail.com>
2020-03-20 19:26:33 -0600

    Add welcome method to game and tested in terminal

--- lib/game.rb
+++ lib/game.rb
@@ -4,4 +4,16 @@ class Game
     @player1 = player1
     @player2 = player2
   end
+
+  def welcome
+    puts "Welcome to War! (or Peace) This game will be played with 52 cards.
+The players today are Megan and Aurora.
+Type 'GO' to start the game!
+------------------------------------------------------------------"
+    input = gets.chomp
+    until input.downcase == 'go'
+      input = gets.chomp
+    end
+  end
+
 end
