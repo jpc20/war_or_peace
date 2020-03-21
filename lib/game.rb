8de47baea7cf21a997a8be690af82a9e7d4a9266
Jack Cullen <jpcullen20@gmail.com>
2020-03-20 19:42:22 -0600

    Add start method and test 1000000 turn limit

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,9 @@
 class Game
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :turn_count
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @turn_count = 0
   end
 
   def welcome
@@ -14,6 +15,15 @@ Type 'GO' to start the game!
     until input.downcase == 'go'
       input = gets.chomp
     end
+  end
+
+  def start
+    welcome()
+
+    until player1.has_lost? || player2.has_lost? || @turn_count == 1000000
+      @turn_count +=1
+    end
+
   end
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -7,43 +7,65 @@ require './lib/turn'
 require "./lib/game"
 
 class GameTest < Minitest::Test
-def test_it_exists
-  card1 = Card.new(:heart, 'Jack', 11)
-  card2 = Card.new(:heart, '10', 10)
-  card3 = Card.new(:heart, '9', 9)
-  card4 = Card.new(:diamond, 'Jack', 11)
-  card5 = Card.new(:heart, '8', 8)
-  card6 = Card.new(:diamond, 'Queen', 12)
-  card7 = Card.new(:heart, '3', 3)
-  card8 = Card.new(:diamond, '2', 2)
-  deck1 = Deck.new([card1, card2, card5, card8])
-  deck2 = Deck.new([card3, card4, card6, card7])
-  player1 = Player.new("Megan", deck1)
-  player2 = Player.new("Aurora", deck2)
+  def test_it_exists
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
 
-  game = Game.new(player1, player2)
+    game = Game.new(player1, player2)
 
-  assert_instance_of Game, game
-end
+    assert_instance_of Game, game
+  end
 
-def test_it_has_readable_attributes
-  card1 = Card.new(:heart, 'Jack', 11)
-  card2 = Card.new(:heart, '10', 10)
-  card3 = Card.new(:heart, '9', 9)
-  card4 = Card.new(:diamond, 'Jack', 11)
-  card5 = Card.new(:heart, '8', 8)
-  card6 = Card.new(:diamond, 'Queen', 12)
-  card7 = Card.new(:heart, '3', 3)
-  card8 = Card.new(:diamond, '2', 2)
-  deck1 = Deck.new([card1, card2, card5, card8])
-  deck2 = Deck.new([card3, card4, card6, card7])
-  player1 = Player.new("Megan", deck1)
-  player2 = Player.new("Aurora", deck2)
+  def test_it_has_readable_attributes
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
 
-  game = Game.new(player1, player2)
+    game = Game.new(player1, player2)
 
-  assert_equal player1, game.player1
-  assert_equal player2, game.player2
-end
+    assert_equal player1, game.player1
+    assert_equal player2, game.player2
+    assert_equal 0, game.turn_count
+  end
+
+  def test_up_to_1000000_turns
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new(player1, player2)
+
+    game.start
+
+    assert_equal 1000000, game.turn_count
+  end
 
 end
