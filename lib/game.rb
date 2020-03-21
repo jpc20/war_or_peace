7ed88ef2f816838292969a9447d996325233c1d9
Jack Cullen <jpcullen20@gmail.com>
2020-03-21 13:58:21 -0600

    Add execute_new_turn and determine winner methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
 class Game
-  attr_reader :player1, :player2, :turn_count
+  attr_reader :player1, :player2, :turn_count, :winner
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @turn_count = 0
+    @winner = nil
   end
 
   def welcome
@@ -17,13 +18,53 @@ Type 'GO' to start the game!
     end
   end
 
+  def execute_new_turn
+    turn = Turn.new(@player1, @player2)
+    @turn_count += 1
+    if turn.type == :basic
+      winner = turn.winner
+      turn.pile_cards
+      turn.award_spoils(winner)
+      p "Turn #{@turn_count}:  #{winner.name} won 2 cards"
+    elsif turn.type == :war
+      if @player1.deck.cards.length < 3
+        @player1.deck.cards.clear
+      elsif @player2.deck.cards.length < 3
+        @player2.deck.cards.clear
+      else
+        winner = turn.winner
+        turn.pile_cards
+        turn.award_spoils(winner)
+        p "Turn #{@turn_count}:  WAR - #{winner.name} won 6 cards"
+      end
+    elsif turn.type == :mutually_assured_destruction
+      turn.pile_cards
+      p "Turn #{@turn_count}: *mutually assured destruction* 6 cards removed from play"
+    end
+  end
+
+  def determine_winner
+    if @player1.has_lost? == true
+      @winner = @player2.name
+      p "*~*~*~* #{@winner} has won the game! *~*~*~*"
+    elsif @player2.has_lost? == true
+      @winner = @player1.name
+      p "*~*~*~* #{@winner} has won the game! *~*~*~*"
+    elsif @turn_count == 1000000
+      p "---- DRAW ----"
+    end
+  end
+
   def start
     welcome()
 
-    until player1.has_lost? || player2.has_lost? || @turn_count == 1000000
-      @turn_count +=1
+    until @player1.has_lost? == true || @player2.has_lost? == true || @turn_count == 1000000
+      execute_new_turn()
     end
 
+    determine_winner
+
   end
+
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -47,25 +47,76 @@ class GameTest < Minitest::Test
     assert_equal 0, game.turn_count
   end
 
-  def test_up_to_1000000_turns
+
+  def test_basic_winner
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
     card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
+    deck1 = Deck.new([card1, card4])
+    deck2 = Deck.new([card2, card3])
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
 
     game = Game.new(player1, player2)
-
     game.start
 
-    assert_equal 1000000, game.turn_count
+    assert_equal "Megan", game.winner
+  end
+
+  def test_war_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:diamond, 'Jack', 11)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, '4', 4)
+    card5 = Card.new(:club, 'King', 13)
+    card6 = Card.new(:spade, 'Ace', 14)
+    deck1 = Deck.new([card1, card4, card5])
+    deck2 = Deck.new([card2, card3, card6])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new(player1, player2)
+    game.start
+
+    assert_equal "Aurora", game.winner
+  end
+
+  def test_mutually_assured_destruction
+    card1 = Card.new(:spade, '10', 10)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:diamond, 'Queen', 12)
+    card6 = Card.new(:club, 'Queen', 12)
+    card7 = Card.new(:diamond, 'Ace', 14)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card4, card5, card7])
+    deck2 = Deck.new([card2, card3, card6, card8])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new(player1, player2)
+    game.start
+
+    assert_equal "Megan", game.winner
+  end
+
+
+  def test_war_on_last_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, '9', 9)
+    deck1 = Deck.new([card1, card4])
+    deck2 = Deck.new([card2, card3])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new(player1, player2)
+    game.start
+
+    assert_equal "Megan", game.winner
   end
 
 end
