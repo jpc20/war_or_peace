a6be6785fd92f09546236dbba504b5ebeb4e6049
Jack Cullen <jpcullen20@gmail.com>
2020-03-22 11:46:47 -0600

    Add ability to read cards.txt and save them to an array

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,10 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
+require './lib/card_generator'
+require 'pry'
 
 class CardGeneratorTest < Minitest::Test
-  def test_it_exists
-    
+  def setup
+    @card_generator = CardGenerator.new('./lib/cards.txt')
   end
+
+  def test_it_exists
+    assert_instance_of CardGenerator, @card_generator
+  end
+
+  def test_it_outputs_cards
+    @card_generator.generate_cards
+    assert_instance_of Card, @card_generator.cards.first
+  end
+
+  def test_it_generates_52_cards
+    @card_generator.generate_cards
+    assert_equal 52, @card_generator.cards.length
+  end
+
 
 end
