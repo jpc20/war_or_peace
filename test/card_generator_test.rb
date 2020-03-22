b1c7b57be413e0a5c172df6d2623b3875f9d67ab
Jack Cullen <jpcullen20@gmail.com>
2020-03-22 12:32:15 -0600

    Add generator to runner file

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -16,11 +16,17 @@ class CardGeneratorTest < Minitest::Test
   def test_it_outputs_cards
     @card_generator.generate_cards
     assert_instance_of Card, @card_generator.cards.first
+    assert_instance_of Card, @card_generator.cards.last
   end
 
   def test_it_generates_52_cards
     @card_generator.generate_cards
     assert_equal 52, @card_generator.cards.length
+  end
+
+  def test_it_can_shuffle
+    @card_generator.generate_cards
+    assert_equal false, @card_generator.cards.shuffle == @card_generator.cards.shuffle
   end
 
 
